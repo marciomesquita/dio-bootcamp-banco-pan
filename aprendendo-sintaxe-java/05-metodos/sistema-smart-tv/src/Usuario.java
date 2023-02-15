51d2f5e24a99e2df5a9847b630ed5c0cc958b766
Marcio Mesquita <botasebatatas@gmail.com>
2023-02-15 13:02:46 -0300

    Criando a Smart TV

---
+++ aprendendo-sintaxe-java/05-metodos/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        
+        SmartTv tv = new SmartTv();
+
+        System.out.println("TV Ligada? " + tv.ligada);
+        System.out.println("Canal Atual? " + tv.canal);
+        System.out.println("Volume? " + tv.volume);
+
+
+    }
+}
